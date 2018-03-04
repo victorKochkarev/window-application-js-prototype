a60710c73aabef256deb54b46d78a30cb77e466e
Victor Kochkarev <victor.kochkarev@gmail.com>
2018-03-04 16:34:48 +0000

    Node Class - exposed editable property list

--- src/script/model/Node.js
+++ src/script/model/Node.js
@@ -2,8 +2,35 @@ class Node{
     constructor(options){
         this.parentNode = options.parentNode;
         this.name = options.name;
-        this.width = options.width;
-        this.height = options.height;
+        this.size = options.size;
+        this.position = options.position;
+
         this.childNodeList = [];
+    }
+
+    propertyList(){
+        let result = [];
+        result.push(
+            new NodeProperty({
+                name: "Name",
+                value: this.name,
+                type: "string"
+            })
+        );
+        result.push(
+            new NodeProperty({
+                name: "Size",
+                value: this.size,
+                type: "size"
+            })
+        );
+        result.push(
+            new NodeProperty({
+                name: "Position",
+                value: this.position,
+                type: "position"
+            })
+        );
+        return result;
     }
 }
